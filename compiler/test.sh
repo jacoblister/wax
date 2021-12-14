1f84724bdf1f12c9fbf8f20a416cc43eda241078
Jacob Lister <jacob.h.lister@gmail.com>
2021-12-15 00:50:50 +1300

    add makefile for wax language tests

--- compiler/test.sh
+++
@@ -1,2 +0,0 @@
-#waxc --ts /tmp/out.ts test/define.wax >/dev/null && tsc /tmp/out.ts && node /tmp/out.js
-waxc --c /tmp/out.c test/define.wax >/dev/null && gcc /tmp/out.c -o /tmp/out.out && /tmp/out.out
